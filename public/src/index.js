825ad0826069fbf56dd1e3b0ed53541946c2d073
nguyenhu21 <35584482+nguyenhu21@users.noreply.github.com>
2021-09-22 19:49:46 -0700

    switching tables between fixtures and standings

---
+++ public/src/index.js
@@ -0,0 +1,23 @@
+const fixtureBtn = document.getElementById("fixture-btn");
+const standingsBtn = document.getElementById("standings-btn");
+const fixtureTable = document.querySelector(".fixture-table");
+const standingsTable = document.querySelector(".standings-table");
+
+//Show fixtures when the fixtures button is pressed
+fixtureBtn.addEventListener('click', () => {
+  console.log("clicked")
+  removeActiveClasses();
+  standingsTable.classList.add('hide');
+});
+
+//Show standings when standings button is pressed
+standingsBtn.addEventListener('click', () => {
+  removeActiveClasses();
+  fixtureTable.classList.add('hide');
+});
+
+//Remove all the classes with active in it
+const removeActiveClasses = () => {
+    fixtureTable.classList.remove("hide");
+    standingsTable.classList.remove("hide");
+}
