5c9b7dac4bc1b2e7ff9adf70ff7086c8db9aca35
nguyenhu21 <35584482+nguyenhu21@users.noreply.github.com>
2021-09-30 20:27:07 -0700

    added creating a new fixture function

--- public/src/index.js
+++ public/src/index.js
@@ -2,6 +2,7 @@ const fixtureBtn = document.getElementById("fixture-btn");
 const standingsBtn = document.getElementById("standings-btn");
 const fixtureTable = document.querySelector(".fixture-table");
 const standingsTable = document.querySelector(".standings-table");
+const seasonSelector = document.getElementById("season-selector");
 
 //Show fixtures when the fixtures button is pressed
 fixtureBtn.addEventListener('click', () => {
@@ -19,4 +20,56 @@ standingsBtn.addEventListener('click', () => {
 const removeActiveClasses = () => {
     fixtureTable.classList.remove("hide");
     standingsTable.classList.remove("hide");
+}
+
+//Add in new fixture
+const createNewFixture = (season) => {
+  //Create new div
+  const newFixture = document.createElement('div');
+
+  /*********************************
+  ****Add in all require attributes*
+  **********************************/
+  
+  //id of this fixture(preset widget)
+  newFixture.setAttribute('id', 'wg-api-football-fixtures');
+
+  //where this data is hosted
+  newFixture.setAttribute('data-host', 'v3.football.api-sports.io');
+
+  //refresh rate of fixture table
+  newFixture.setAttribute('refresh-rate', '60');
+
+  //today's date
+  newFixture.setAttribute('data-date', '');
+
+  //league to display for fixture
+  newFixture.setAttribute('data-league', '39');
+
+  //team to display for
+  newFixture.setAttribute('data-team', '');
+
+  //season
+  newFixture.setAttribute('data-season', season);
+
+  //number of previous matches to display
+  newFixture.setAttribute('data-last', '');
+
+  //number of next matches to display
+  newFixture.setAttribute('data-next', '');
+
+  //API key (SHOULD BE INVISIBLE/SECURE)
+  newFixture.setAttribute('data-key', '0faf0093c15b42c79332d719ba6724f9');
+
+  //theme of table (dark, light) default is light
+  newFixture.setAttribute('data-theme', '');
+
+  //show error
+  newFixture.setAttribute('data-show-errors', 'false');
+
+  //class of div
+  newFixture.setAttribute('class', 'api_football_loader');
+
+  //Return the new fixture
+  return newFixture;
 }
